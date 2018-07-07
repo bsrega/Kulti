3d5b7e878a73ff31079bf0146653d8ca479b691f
bsrega <stephan_brun@hotmail.com>
2018-07-07 10:20:10 +0200

    Add files via upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Mvc4_Kulti.Filters;
+
+namespace Mvc4_Kulti
+{
+  public class FilterConfig
+  {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+    {
+      filters.Add(new HandleErrorAttribute());
+      filters.Add(new InitializeSimpleMembershipAttribute());
+      filters.Add(new HandleErrorAttribute());
+
+    }
+  }
+}
